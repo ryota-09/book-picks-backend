dafb2037e2358457d835dc2251fa5a29b86dfe38
ryota-09 <n.ryo950911@gmail.com>
2022-04-29 13:16:21 +0900

    scrapingディレクトリ作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScrapingModule } from './scraping/scraping/scraping.module';
 
 @Module({
-  imports: [],
+  imports: [ScrapingModule],
   controllers: [AppController],
   providers: [AppService],
 })
