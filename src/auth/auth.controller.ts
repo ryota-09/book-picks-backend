8b5432e9b11f4d28c2fb83baae5916d700448e5d
ryota-09 <n.ryo950911@gmail.com>
2022-05-22 13:42:52 +0900

    authControllerの実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/db/dto/create-user.dto';
+
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() createUser: CreateUserDto) {
+    return this.authService.login(createUser);
+  }
+}
