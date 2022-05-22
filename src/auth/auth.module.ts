1e9391047380a039eb166f5f05a41cc2ab04c11b
ryota-09 <n.ryo950911@gmail.com>
2022-05-22 13:23:16 +0900

    authmodule設定

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DbModule } from '../db/db.module';
+import { JwtStrategy } from './auth.strategy';
 
 @Module({
+  imports: [
+    DbModule,
+    JwtModule.register({
+      secret: process.env.AUTH_STRATEGY_SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
