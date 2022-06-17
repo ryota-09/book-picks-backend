2b23650f87a76cb562e5b10ec16cda48ff541535
ryota-09 <n.ryo950911@gmail.com>
2022-06-17 14:31:30 +0900

    スクレイピングのためのherokuのオプション

--- src/scraping/scraping/scraping.service.ts
+++ src/scraping/scraping/scraping.service.ts
@@ -21,6 +21,7 @@ export class ScrapingService {
 
     const browser = await puppeteer.launch({
       headless: false,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
 
